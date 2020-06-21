618499e5eebc37e5621b82f206e2f53d633c1703
Jakub <jakub@polystream.com>
2020-06-21 19:47:41 +0100

    added hashes and xxh3

--- Playground/hash.cpp
+++ Playground/hash.cpp
@@ -1,4 +1,5 @@
 #include "hash.h"
+#include <xxhash/xxh3.h>
 
 namespace Hash
 {
@@ -6,4 +7,12 @@ namespace Hash
 	template<> u64 HashValue(i64 x) { return x; }
 	template<> u64 HashValue(u32 x) { return x; }
 	template<> u64 HashValue(u64 x) { return x; }
+
+	u64 HashMemory(const void* ptr, i64 size) {
+		return XXH3_64bits(ptr, size);
+	}
+
+	u64 HashMemoryWithSeed(const void* ptr, i64 size, u64 seed) {
+		return XXH3_64bits_withSeed(ptr, size, seed);
+	}
 }
--- Playground/hash.h
+++ Playground/hash.h
@@ -5,10 +5,18 @@
 namespace Hash
 {
 	template<typename T>
-	u64 HashValue(T x);
+	u64 HashValue(T);
 
-	template<> u64 HashValue(i32 x);
-	template<> u64 HashValue(i64 x);
-	template<> u64 HashValue(u32 x);
-	template<> u64 HashValue(u64 x);
+	template<> u64 HashValue(i32);
+	template<> u64 HashValue(i64);
+	template<> u64 HashValue(u32);
+	template<> u64 HashValue(u64);
+
+	template<typename T> u64 HashValue(T* x)
+	{
+		return HashValue(reinterpret_cast<u64>(x));
+	}
+
+	u64 HashMemory(const void*, i64);
+	u64 HashMemoryWithSeed(const void*, i64, u64);
 }
