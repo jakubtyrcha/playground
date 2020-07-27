864897706a479131366a843f1b0bffa4afadc7d2
Jakub <jakub@polystream.com>
2020-07-27 01:53:22 +0100

    extended random with ranged ints

--- src/lib/random.cpp
+++ src/lib/random.cpp
@@ -1,41 +1,52 @@
 #include "pch.h"
 
 #include "random.h"
-#include <mersennetwister/mersenne-twister.h>
 #include <limits>
+#include <mersennetwister/mersenne-twister.h>
 
 namespace Random {
-	Generator::Generator(u32 seed) {
-		mt_init(reinterpret_cast<MTState**>(&state_), seed);
-	}
+Generator::Generator(u32 seed)
+{
+    mt_init(reinterpret_cast<MTState**>(&state_), seed);
+}
 
-	Generator::~Generator() {
-		mt_destroy(static_cast<MTState*>(state_));
-		state_ = nullptr;
-	}
+Generator::~Generator()
+{
+    mt_destroy(static_cast<MTState*>(state_));
+    state_ = nullptr;
+}
 
-	u32 Generator::U32Uniform() {
-		return mt_rand_u32(static_cast<MTState*>(state_));
-	}
+u32 Generator::U32Uniform()
+{
+    return mt_rand_u32(static_cast<MTState*>(state_));
+}
 
-	f32 Generator::F32Uniform() {
-		constexpr f32 epsilon = FLT_EPSILON;
-		constexpr u32 resolution = static_cast<u32>(1.f / epsilon);
-		constexpr u32 buckets = UINT32_MAX / resolution;
-		constexpr u32 max_value = resolution * buckets - 1;
+i32 Generator::I32UniformInRange(i32 from, i32 to)
+{
+    // todo: not uniform, but pretty close if the span is relatively small
+    return from + U32Uniform() % (to - from);
+}
 
-		u32 r;
-		while(true) {
-			r = U32Uniform();
-			if(r <= max_value) {
-				break;
-			}
-		}
+f32 Generator::F32Uniform()
+{
+    constexpr f32 epsilon = FLT_EPSILON;
+    constexpr u32 resolution = static_cast<u32>(1.f / epsilon);
+    constexpr u32 buckets = UINT32_MAX / resolution;
+    constexpr u32 max_value = resolution * buckets - 1;
 
-		return (r % resolution) * epsilon;
-	}
+    u32 r;
+    while (true) {
+        r = U32Uniform();
+        if (r <= max_value) {
+            break;
+        }
+    }
 
-	f32 Generator::F32UniformInRange(f32 from, f32 to) {
-		return to + (from - to) * F32Uniform();
-	}
+    return (r % resolution) * epsilon;
+}
+
+f32 Generator::F32UniformInRange(f32 from, f32 to)
+{
+    return to + (from - to) * F32Uniform();
+}
 }
--- src/lib/random.h
+++ src/lib/random.h
@@ -11,6 +11,7 @@ struct Generator : private Core::Pinned<Generator> {
     ~Generator();
 
     u32 U32Uniform();
+    i32 I32UniformInRange(i32 from, i32 to);
     f32 F32Uniform(); // [0,1)
     f32 F32UniformInRange(f32 from, f32 to);
 };
