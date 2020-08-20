56fe04b97483089e38c24504a84d6872f4ba3c18
Jakub <jakub@polystream.com>
2020-08-20 21:14:33 +0100

    slice (linear memory view)

---
+++ source/include/core/Slice.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Types.h"
+#include "algorithms.h"
+
+namespace Playground {
+
+template <typename T>
+struct Slice {
+    T* data;
+    i64 num;
+
+    T& operator[](i64 index)
+    {
+        plgr_assert(index == Clamp(index, 0ll, num - 1));
+        return data[index];
+    }
+};
+
+}
