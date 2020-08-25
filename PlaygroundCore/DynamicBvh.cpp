55983419d7d987871c5c053224da9d2e9130126f
Jakub <jakub@polystream.com>
2020-08-26 00:52:27 +0100

    wip rotations

--- PlaygroundCore/DynamicBvh.cpp
+++ PlaygroundCore/DynamicBvh.cpp
@@ -29,6 +29,17 @@ namespace Playground {
         return 1;
     }
 
+    i32 DynamicBvh::Node::GetChildIndex(i32 i) const {
+        if (children[0] == i) {
+            return 0;
+        }
+        if (children[1] == i) {
+            return 1;
+        }
+        plgr_assert(false);
+        return -1;
+    }
+
 	Handle DynamicBvh::Add(Aabb3D bounds) {
 		if(root_ == NULL_NODE) {
             i32 node = nodes_freelist_.Allocate();
@@ -134,6 +145,107 @@ namespace Playground {
         }
     }
 
+    f32 DynamicBvh::_GetMergeCost(i32 l, i32 r) const {
+        Aabb3D merged = nodes_[l].bounds;
+        plgr_assert(l != NULL_NODE);
+        if(r != NULL_NODE) {
+            merged = merged.Union(nodes_[r].bounds);
+        }
+        return merged.Area();
+    }
+
+    f32 DynamicBvh::_GetMergeCost(i32 i, i32 j, i32 k) const {
+        plgr_assert(i != NULL_NODE && j != NULL_NODE);
+        Aabb3D merged = nodes_[i].bounds.Union(nodes_[j].bounds);
+        if(k != NULL_NODE) {
+            merged = merged.Union(nodes_[k].bounds);
+        }
+        return merged.Area();
+    }
+
+    void DynamicBvh::Rotate(i32 index) {
+        if(nodes_[index].ChildrenNum() < 2) {
+            return;
+        }
+
+        i32 l = nodes_[index].children[0];
+        i32 r = nodes_[index].children[1];
+
+        if(nodes_[l].ChildrenNum() < nodes_[r].ChildrenNum()) {
+            // make sure l has 2 children, less fuss later
+            Swap(l, r);
+            Swap(nodes_[index].children[0], nodes_[index].children[1]);
+        }
+
+        i32 l0 = nodes_[l].children[0];
+        i32 l1 = nodes_[l].children[1];
+
+        i32 r0 = nodes_[r].children[0];
+        i32 r1 = nodes_[r].children[1];
+
+        f32 AB = _GetMergeCost(l0, l1);
+        f32 CD = _GetMergeCost(r0, r1);
+        f32 AC = _GetMergeCost(l0, r0);
+        f32 AD = _GetMergeCost(l0, r1);
+        f32 BC = _GetMergeCost(l1, r0);
+        f32 BD = _GetMergeCost(l1, r1);
+
+        f32 ABC = _GetMergeCost(l0, l1, r0);
+        f32 ABD = _GetMergeCost(l0, l1, r1);
+        f32 ACD = _GetMergeCost(l0, r0, r1);
+        f32 BCD = _GetMergeCost(l1, r0, r1);
+
+        f32 cost_noop = AB + CD;
+        f32 cost_swap_l0r0 = BC + AD;
+        f32 cost_swap_l1r0 = AC + BD;
+        f32 cost_swap_lr0 = AB + ABD;
+        f32 cost_swap_lr1 = AB + ABC;
+        f32 cost_swap_l0r = CD + BCD;
+        f32 cost_swap_l1r = CD + ACD;
+
+        f32 min_cost = Min(Min(cost_noop, Min(cost_swap_l0r0, cost_swap_l1r0)), Min(Min(cost_swap_lr0, cost_swap_lr1), Min(cost_swap_l0r, cost_swap_l1r)));
+
+        if(cost_noop == min_cost) {
+        }
+        else if(cost_swap_l0r0 == min_cost) {
+            _Rotate(l0, r0);
+        }
+        else if(cost_swap_l1r0 == min_cost) {
+            _Rotate(l1, r0);
+        }
+        else if(cost_swap_lr0 == min_cost) {
+            _Rotate(l, r0);
+        }
+        else if(cost_swap_lr1 == min_cost) {
+            _Rotate(l, r1);
+        }
+        else if(cost_swap_l0r == min_cost) {
+            _Rotate(l0, r);
+        }
+        else if(cost_swap_l1r == min_cost) {
+            _Rotate(l1, r);
+        }
+    }
+
+    void DynamicBvh::_Rotate(i32 index0, i32 index1) {
+        i32 parent0 = nodes_[index0].parent;
+        i32 parent1 = nodes_[index1].parent;
+        plgr_assert(parent0 != NULL_NODE && parent1 != NULL_NODE);
+        plgr_assert(parent0 != parent1);
+
+        i32 child0 = nodes_[parent0].GetChildIndex(index0);
+        i32 child1 = nodes_[parent1].GetChildIndex(index1);
+
+        // swap
+        nodes_[parent0].children[child0] = index1;
+        nodes_[index1].parent = parent0;
+        nodes_[parent1].children[child1] = index0;
+        nodes_[index0].parent = parent1;
+
+        _Refit(parent0);
+        _Refit(parent1);
+    }
+
     void DynamicBvh::_Refit(i32 index) {
 
         while (index != NULL_NODE) {
@@ -192,10 +304,6 @@ namespace Playground {
         _Detach(remove_index);
         _TrimNode(parent);
         nodes_freelist_.Free(remove_index);
-    }
-
-    void DynamicBvh::Modify(Handle h, Aabb3D aabb) {
-        
     }
 
     i32 DynamicBvh::GetDepth() const {
--- source/include/core/DynamicBvh.h
+++ source/include/core/DynamicBvh.h
@@ -17,6 +17,7 @@ struct DynamicBvh {
         bool IsLeaf() const;
         i32 ChildrenNum() const;
         i32 FreeChildIndex() const;
+        i32 GetChildIndex(i32) const;
     };
 
     static constexpr i32 NULL_NODE = -1;
@@ -26,12 +27,16 @@ struct DynamicBvh {
     i32 root_ = NULL_NODE;
 
     Handle Add(Aabb3D bounds);
-    void Modify(Handle, Aabb3D);
     void Remove(Handle);
 
     void _Detach(i32);
     void _TrimNode(i32);
     void _Refit(i32);
+    void _Rotate(i32, i32);
+    void Rotate(i32);
+
+    f32 _GetMergeCost(i32 l, i32 r) const;
+    f32 _GetMergeCost(i32 i, i32 j, i32 k) const;
 
     i32 GetDepth() const;
 
